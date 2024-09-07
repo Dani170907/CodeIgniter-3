a15babec8873eff529170c16fdb35e09c5718630
Dani170907 <daniramadhan170907@gmail.com>
2024-09-07 10:08:20 +0700

    menyesuaikan css dengan input dan edit data

--- application/views/editStudent.php
+++ application/views/editStudent.php
@@ -9,36 +9,25 @@
     
     <h1>Edit Data</h1>
 
-    <table border="1px">
-        <tr>
-            <th>No.</th>
-            <th>Name</th>
-            <th>NIS</th>
-            <th>Status</th>
-            <th>Address</th>
-            <th>Date Of Birth</th>
-        </tr>
-    </table>
+        
+    <div>
+    <form action="/action_page.php">
+        <label for="fname">First Name</label>
+        <input type="text" id="fname" name="firstname" placeholder="Your name..">
 
+        <label for="lname">Last Name</label>
+        <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+        <label for="country">Country</label>
+        <select id="country" name="country">
+        <option value="australia">Australia</option>
+        <option value="canada">Canada</option>
+        <option value="usa">USA</option>
+        </select>
     
-<div>
-  <form action="/action_page.php">
-    <label for="fname">First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name..">
-
-    <label for="lname">Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-
-    <label for="country">Country</label>
-    <select id="country" name="country">
-      <option value="australia">Australia</option>
-      <option value="canada">Canada</option>
-      <option value="usa">USA</option>
-    </select>
-  
-    <input type="submit" value="Submit">
-  </form>
-</div>
+        <input type="submit" value="Submit">
+    </form>
+    </div>
 
     <a href="">Save Data</a>
 </body>
