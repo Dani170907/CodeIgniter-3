e37d690cf4fd624941ee5134b3c3b5b0b27f4225
Dani170907 <daniramadhan170907@gmail.com>
2024-09-07 10:03:01 +0700

    update all

---
+++ application/views/templates/headerLogin.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url('assets/login.css')?>">
+    <title>LOGIN</title>
+</head>
+<body>
