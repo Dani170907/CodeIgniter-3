d8411c1487604d3fee7605fdf17662d5878ce536
Dani170907 <daniramadhan170907@gmail.com>
2024-09-09 10:25:44 +0700

    membuat form untu kview login.php

--- application/views/login.php
+++ application/views/login.php
@@ -1,2 +1,10 @@
 <h1>Log In Page</h1>
-<button>Log Out</button>
+
+<form action="<?= base_url() ?>Login/verification" method="post">
+    <label for="username">UserName</label>
+    <input type="text" name="username" placeholder="Masukkan UserName">
+    <br><br>
+    <label for="password">Password</label>
+    <input type="password" name="password" id="" placeholder="Masukkan Password"><br><br>
+    <button type="submit">Log In</button>
+</form>
