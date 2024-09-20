61bc24bd1507995cb930661346e1879ba234a1ef
Dani170907 <daniramadhan170907@gmail.com>
2024-09-20 08:51:19 +0700

    menambahkan templates baru navbar.php

--- application/views/templates/headerEdit.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url('assets/input.css')?>">
-    <title>Edit</title>
-</head>
-<body>
---
+++ application/views/templates/navbar.php
@@ -0,0 +1,4 @@
+<div class="navbar">
+    <a href="<?= base_url(); ?>">admin</a>
+    <a href="<?= base_url(); ?>">login/logout</a>
+</div>
