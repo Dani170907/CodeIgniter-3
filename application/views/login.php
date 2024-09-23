1181d60c3896b182812d3dc547769e90456570ec
Dani170907 <daniramadhan170907@gmail.com>
2024-09-23 08:19:44 +0700

    mengaktifkan session di config/autoload.php

--- application/views/login.php
+++ application/views/login.php
@@ -1,5 +1,9 @@
 <h1>Log In Page</h1>
 
+
+<?= $this->session->flashdata('message') ?>
+
+<h2>Form Login</h2>
 <form action="<?= base_url() ?>Login/authenticate" method="post">
     <label for="username">UserName</label>
     <input type="text" name="username" placeholder="Masukkan UserName">
